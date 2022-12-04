22b1ada6b573232b0a1f311547cc8dedd4e4c97d
NachoCape <igcapezzolo@hotmail.com>
2022-12-04 16:27:48 +0000

    fix documented module task0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,26 +8,27 @@ import sys
 if __name__ == '__main__':
     """0. Gather data from an API"""
 
-
-user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                    format(sys.argv[1]))
-user = user.json()
-todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                     format(sys.argv[1]))
-todos = todos.json()
-# print(todos.json())
-name = user['name']
-total_tasks = len(todos)
-tasks_done = 0
-lists_of_titles = []
-for f in todos:
-    if user['id'] == f['userId']:
-        total_tasks += 1
-        if f['completed']:
-            tasks_done += 1
-            lists_of_titles.append(f['title'])
-    # print(f"****{f}****")
-print("Employee {} is done with tasks({}/{})".
-      format(name, tasks_done, total_tasks))
-for title in lists_of_titles:
-    print("\t{}".format(title))
+    user = requests.\
+        get('https://jsonplaceholder.typicode.com/users/{}'.
+            format(sys.argv[1]))
+    user = user.json()
+    todos = requests.\
+        get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+            format(sys.argv[1]))
+    todos = todos.json()
+    # print(todos.json())
+    name = user['name']
+    total_tasks = len(todos)
+    tasks_done = 0
+    lists_of_titles = []
+    for f in todos:
+        if user['id'] == f['userId']:
+            total_tasks += 1
+            if f['completed']:
+                tasks_done += 1
+                lists_of_titles.append(f['title'])
+        # print(f"****{f}****")
+    print("Employee {} is done with tasks({}/{})".
+          format(name, tasks_done, total_tasks))
+    for title in lists_of_titles:
+        print("\t{}".format(title))
