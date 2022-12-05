e376ea2f09fb76b057cf1afb0ddd9d6185657cbe
NachoCape <igcapezzolo@hotmail.com>
2022-12-05 00:09:00 +0000

    add task3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""task0 module"""
+
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    """0. Gather data from an API"""
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users/')
+    user = user.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    todos = todos.json()
+    dicty = {}
+    _list = []
+    for i in user:
+        for j in todos:
+            if j.get('userId') == i.get('id'):
+                _list += [{"username": i.get('username'),
+                          "completed": j.get('completed'),
+                           "task": j.get('title')}]
+        dicty["{}".format(i.get('id'))] = _list
+    with open("todo_all_employees.json", 'w', encoding="UTF-8") as file:
+        w = json.dump(dicty, file)
